95283735b6f5028dd7b86824467f3abce2277f9f
transsuperfreak <168235283+transsuperfreak@users.noreply.github.com>
2024-05-04 05:19:52 +0200

    Create init.js

---
+++ init.js
@@ -0,0 +1,18 @@
+// Grab this code, create a new document in /a/boot, name it 'init.js' and open it, paste this code and save. Restart your system and you can find "free robux 2024" in programs (win93), or programs => other (win94)
+
+le._apps.boot = {
+  categories: "Amusement",
+  name: "Free Robux 2024",
+  icon: "/c/files/images/icons/pow.png",
+  exec: function () {
+
+    $confirm("Warning! this will destroy windows 93 you sure??", function (ok) {
+      if (!ok) {
+        $alert("Good choice.");
+      } else {
+        $alert("Prepare. Once you refresh, you shall never use the system again.");
+        $file.save("/a/boot/win94.js",'$kernel.on("splash:ready", function() { function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); } sleep(3500).then(() => { $window({ title: "Suffer", html: "CREDIT TO SUBZERO FOR FIXING MY CODE", icon: "c/sys/skins/w93/apps/hexedit.png", resizable: false, maximizable: false }); $window({ title: "Suffer", html: "CREDIT TO SUBZERO FOR FIXING MY CODE", icon: "c/sys/skins/w93/apps/hexedit.png", resizable: false, maximizable: false }); $window({ title: "Suffer", html: "CREDIT TO SUBZERO FOR FIXING MY CODE", icon: "c/sys/skins/w93/apps/hexedit.png", resizable: false, maximizable: false }); $window({ title: "Suffer", html: "<p><b>CREDIT TO SUBZERO FOR FIXING MY CODE</b></p>", icon: "c/sys/skins/w93/apps/hexedit.png", resizable: false, maximizable: false }); timeout = setTimeout(doom, 1000); function doom() { window.location.href = "https://windows93.net/derp.html";; } })});');
+      }
+    });
+  }
+};
